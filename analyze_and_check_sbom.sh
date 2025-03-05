e7524ffa7a5d065696c79b5d69776789623bd5df
Martin Zehetmayer <martin.zehetmayer@maibornwolff.de>
2025-03-05 10:42:26 +0100

    chore: added more pre-commit hooks for security and convient

--- analyze_and_check_sbom.sh
+++ analyze_and_check_sbom.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-sudo /sbom-utility license list --input-file=$SBOM_PATH --format=csv --summary --config-license=$LICENSE_POLICY_PATH -o=evaluated_sbom.csv
-python3 /check_usage_policy.py --pipelinebreak $BREAK_ENABLED evaluated_sbom.csv
+# shellcheck shell=dash
+
+if [ -z "$SBOM_PATH" ] || [ -z "$LICENSE_POLICY_PATH" ] || [ -z "$BREAK_ENABLED" ]; then
+  echo "Missing variables in check script. Please check SBOM_PATH or LICENSE_POLICY_PATH or BREAK_ENABLED"
+  exit 1
+fi
+sudo /sbom-utility license list --input-file="$SBOM_PATH" --format=csv --summary --config-license="$LICENSE_POLICY_PATH" -o=evaluated_sbom.csv
+python3 /check_usage_policy.py --pipelinebreak "$BREAK_ENABLED" evaluated_sbom.csv
